416528e31bbeaf70e7b775370198ffaee2ec9955
Tony <AnthonySimmons99@gmail.com>
2015-01-17 15:46:21 -0800

    Add Card and Deck classes.

---
+++ Card.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+class Card:
+	'Represents a playing card'
+
+	Value = 0
+	Suit = ''
+	
+
+	def __init__(self, Suit, Value):
+		self.Suit = Suit
+		self.Value = Value
+
+
+	def __str__():
+		
---
+++ Deck.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+class Deck:
+	'Class to represent a Deck of Cards'
+
+	Cards = []
+
+	def __init__():
+		InitCards()
+		ShuffleCards()
+
+
+	def InitCards():
+		Cards = []
